cbce4df7b592fbd973b90b9922c3dfe45ef14e8b
s3nki69 <tarnok.zoltan@gmail.com>
2026-01-26 20:20:21 +0100

    Update MainPagerAdapter.kt

--- app/src/main/java/hu/hoc/app/MainPagerAdapter.kt
+++ app/src/main/java/hu/hoc/app/MainPagerAdapter.kt
@@ -6,13 +6,15 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class MainPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
     
-    override fun getItemCount(): Int = 3
+    // Most már 4 fülünk van
+    override fun getItemCount(): Int = 4
     
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> ArticlesFragment()
             1 -> CouponsFragment()
             2 -> VideosFragment()
+            3 -> TikTokFragment() // Itt az új TikTok fül
             else -> ArticlesFragment()
         }
     }
