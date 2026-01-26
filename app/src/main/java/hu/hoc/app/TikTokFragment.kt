a4f34c83d2c0074789cf9865b87851190cece7d2
s3nki69 <tarnok.zoltan@gmail.com>
2026-01-26 20:27:22 +0100

    Create TikTokFragment.kt

---
+++ app/src/main/java/hu/hoc/app/TikTokFragment.kt
@@ -0,0 +1,79 @@
+package hu.hoc.app
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+
+class TikTokFragment : Fragment() {
+
+    private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
+
+    // A TikTok profilod címe
+    private val TIKTOK_URL = "https://www.tiktok.com/@hoc.hu"
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Fontos: Itt a tiktok layoutot töltjük be
+        return inflater.inflate(R.layout.fragment_tiktok, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        webView = view.findViewById(R.id.tiktokWebView)
+        progressBar = view.findViewById(R.id.progressBar)
+
+        setupWebView()
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    isEnabled = false
+                    requireActivity().onBackPressed()
+                }
+            }
+        })
+
+        webView.loadUrl(TIKTOK_URL)
+    }
+
+    private fun setupWebView() {
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webSettings.userAgentString = "Mozilla/5.0 (Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+        
+        webSettings.cacheMode = WebSettings.LOAD_DEFAULT
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.visibility = View.VISIBLE
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility = View.GONE
+            }
+        }
+    }
+}
